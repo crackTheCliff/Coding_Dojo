73b25c04aaefe203aa5332bcdbbc3ab6f534fae1
Cliff Helms <cliff.helms.2@gmail.com>
2022-09-13 12:23:22 -0500

    initial_commit

---
+++ Week-1/Day-2/Algos/file.js
@@ -0,0 +1,42 @@
+/**
+ * Concatenate three given strings together in this order:
+ * 3rd string, 1st string, 2nd string.
+ * @returns {string} The three given strings combined (concatenated).
+ */
+function combineStrings(str1, str2, str3) {
+    var string = str3 + str1 + str2;
+    return string;
+}
+console.log(combineStrings("pizza", "I", "love"))
+
+/**
+ * Combine two given strings together with the smaller string being added
+ * in the front. If they are the same length, the first string will be
+ * added to the front.
+ * @returns {string} The combined strings.
+ */
+function combineSmallerStringFirst(str1, str2) {
+    if (str1.length > str2.length) {
+        return str2 + str1;
+    }
+
+    else {
+        return str1 + str2;
+    }
+}
+
+/**
+ * Given a string and an integer representing how many times the string should
+ * be repeated, create a new string that is the given string repeated that
+ * many times.
+ * @param {string} text The given string to be repeated.
+ * @param {number} num The number of times to repeat the given string.
+ * @returns {string} The given string repeated the specified amount of times.
+ */
+function stringRepeat(text, num) {
+    var ans = ''
+    for (let i = 0; i < num; i++) {
+        ans += text
+    }
+    return ans
+}
