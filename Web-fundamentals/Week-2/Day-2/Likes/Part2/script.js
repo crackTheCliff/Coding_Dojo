60bd0a03483074edd1046818d2d17d423dbf0095
Cliff Helms <cliff.helms.2@gmail.com>
2022-09-20 17:42:51 -0500

    modify_js

--- Web-fundamentals/Week-2/Day-2/Likes/Part2/script.js
+++ Web-fundamentals/Week-2/Day-2/Likes/Part2/script.js
@@ -1,28 +1,5 @@
-var count1 = 0;
-var count1Element = document.querySelector("#count1");
 
-function addLikes() {
-    count1++;
-    count1Element.innerText = count1 + " like(s)";
-    console.log(count1);
-}
-
-
-var count2 = 0;
-var count2Element = document.querySelector("#count2");
-
-function addLikes2() {
-    count2++;
-    count2Element.innerText = count2 + " like(s)";
-    console.log(count2);
-}
-
-
-var count3 = 0;
-var count3Element = document.querySelector("#count3");
-
-function addLikes3() {
-    count3++;
-    count3Element.innerText = count3 + " like(s)";
-    console.log(count3);
+function addLikes(element) {
+    var h2 = element.parentNode.querySelector("h2")
+    h2.innerText++;
 }
