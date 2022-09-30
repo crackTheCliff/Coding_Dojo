969f64df97bfd6f8d69d6742c74540d6035b8307
Cliff Helms <cliff.helms.2@gmail.com>
2022-09-30 17:57:15 -0500

    initial_commit

---
+++ Python/flask/fundamentals/counter/server.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, redirect, session
+app = Flask(__name__)
+app.secret_key = 'the force is with you'
+
+@app.route('/')
+def index():
+    if "count" in session:
+        session["count"] + 1
+    else:    
+        session["count"] = 1
+    return render_template("counter.html")
+
+@app.route("/count", methods=["POST"])
+def view_count():
+    if request.form["alter"]=="add":
+        session["count"] += 1
+    elif request.form["alter"]=="reset":
+        session["count"] = 1
+    return redirect("/")
+
+@app.route("/destroy")
+def destroy():
+    session.clear()
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(debug = True)
