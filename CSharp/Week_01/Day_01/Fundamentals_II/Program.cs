75971c1d1eaca547d184a2632c903ec8243b56a5
Cliff Helms <cliff.helms.2@gmail.com>
2022-11-28 17:11:59 -0600

    initial_commit

---
+++ CSharp/Week_01/Day_01/Fundamentals_II/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Three Basic Arrays
+int[] intArray = { 0, 1, 2, 4, 5, 6, 7, 8, 9 };
+
+string[] strArray = { "Tim", "Martin", "Nikki", "Sara" };
+
+bool[] boolArray = new bool[5];
+for (int i = 1; i < boolArray.Length; i++)
+{
+    if (i % 2 == 0)
+    {
+        boolArray[i] = true;
+    }
+}
+foreach (bool item in boolArray)
+{
+    Console.WriteLine(item);
+}
+
+// List of Flavors
+List<string> flavors = new List<string>();
+flavors.Add("Vanilla");
+flavors.Add("Chocolate");
+flavors.Add("Strawberry");
+flavors.Add("Cookies and Cream");
+flavors.Add("Caramel Swirl");
+// Console.WriteLine(flavors.Count);
+// Console.WriteLine(flavors[2]);
+// flavors.RemoveAt(2);
+// Console.WriteLine(flavors.Count);
+
+// User Dictionary
+Random rand = new Random();
+Dictionary<string, string> user = new Dictionary<string, string>();
+foreach(string name in strArray)
+{
+    string randomFlavor = flavors[rand.Next(0, flavors.Count)];
+    user.Add(name, randomFlavor);
+}
+
+foreach (KeyValuePair<string, string> entry in user)
+{
+    Console.WriteLine($"{entry.Key}: {entry.Value}");
+}
