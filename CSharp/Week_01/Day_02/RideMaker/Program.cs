b405c29a92754efae1eb50290708afd5e2817c61
Cliff Helms <cliff.helms.2@gmail.com>
2022-11-29 21:19:35 -0600

    initial_commit

---
+++ CSharp/Week_01/Day_02/RideMaker/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+Vehicle car = new Vehicle("VW GTI", "Green");
+Vehicle truck = new Vehicle("Ford Ranger", "Red");
+Vehicle pos = new Vehicle("Toyota Corolla", "Silver");
+Vehicle magic = new Vehicle("Bugatti", "Purple", 2, true, 300);
+
+List<Vehicle> vehicles = new List<Vehicle>() {car, truck, pos, magic};
+
+foreach (Vehicle item in vehicles)
+{
+    item.ShowInfo();
+}
+
+car.ShowInfo();
+car.Travel(100);
+car.ShowInfo();
