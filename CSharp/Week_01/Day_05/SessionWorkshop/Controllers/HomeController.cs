4fdefa078eea480de990e1d9ae0e2a9ed6c182bb
Cliff Helms <cliff.helms.2@gmail.com>
2022-12-05 10:22:23 -0600

    Initial_commit

---
+++ CSharp/Week_01/Day_05/SessionWorkshop/Controllers/HomeController.cs
@@ -0,0 +1,94 @@
+﻿
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using SessionWorkshop.Models;
+
+namespace SessionWorkshop.Controllers;
+
+public class HomeController : Controller
+{
+    [HttpGet("")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpGet("buttons")]
+    public IActionResult ButtonPage()
+    {
+        string username = HttpContext.Session.GetString("Username");
+        if (username != null)
+        {
+            return View("ButtonPage");
+        }
+        else
+        {
+            return RedirectToAction("Index");
+        }
+    }
+
+    [HttpPost("submit")]
+    public IActionResult Submit(string Name)
+    {
+        HttpContext.Session.SetString("Username", Name);
+        HttpContext.Session.SetInt32("Count", 22);
+        return RedirectToAction("ButtonPage");
+    }
+
+    [HttpPost("logout")]
+    public IActionResult Logout()
+    {
+        HttpContext.Session.Clear();
+        return RedirectToAction("Index");
+    }
+
+    [HttpPost("plusone")]
+    public IActionResult PlusOne()
+    {
+        int? count = HttpContext.Session.GetInt32("Count");
+        if (count == null)
+        {
+            count = 0;
+        }
+        count += 1;
+        HttpContext.Session.SetInt32("Count", (int)count);
+        return RedirectToAction("ButtonPage");
+    }
+    [HttpPost("minusone")]
+    public IActionResult MinusOne()
+    {
+        int? count = HttpContext.Session.GetInt32("Count");
+        if (count == null)
+        {
+            count = 0;
+        }
+        count -= 1;
+        HttpContext.Session.SetInt32("Count", (int)count);
+        return RedirectToAction("ButtonPage");
+    }
+    [HttpPost("timestwo")]
+    public IActionResult TimesTwo()
+    {
+        int? count = HttpContext.Session.GetInt32("Count");
+        if (count == null)
+        {
+            count = 0;
+        }
+        count *= 2;
+        HttpContext.Session.SetInt32("Count", (int)count);
+        return RedirectToAction("ButtonPage");
+    }
+    [HttpPost("random")]
+    public IActionResult Random()
+    {
+        Random rand = new Random();
+        int? count = HttpContext.Session.GetInt32("Count");
+        if (count == null)
+        {
+            count = 0;
+        }
+        count = count + rand.Next(1, 11);
+        HttpContext.Session.SetInt32("Count", (int)count);
+        return RedirectToAction("ButtonPage");
+    }
+}
