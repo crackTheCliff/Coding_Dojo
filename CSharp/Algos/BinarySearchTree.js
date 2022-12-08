4e8b42d1d78fe08780a892e831e95a3ea69ef2be
Cliff Helms <cliff.helms.2@gmail.com>
2022-12-07 19:33:36 -0600

    initial_commit

--- CSharp/Algos/BinarySearchTree.js
+++ CSharp/Algos/BinarySearchTree.js
@@ -39,26 +39,46 @@ class BinarySearchTree {
     }
 
     /**
-     * Determines if this tree contains the given searchVal.
+     * Inserts a new node with the given newVal in the right place to preserver
+     * the order of this tree.
      * - Time: O(?).
      * - Space: O(?).
+     * @param {number} newVal The data to be added to a new node.
+     * @returns {BinarySearchTree} This tree.
+     */
+    insert(newVal) { }
+
+    /**
+     * Inserts a new node with the given newVal in the right place to preserver
+     * the order of this tree.
+     * - Time: O(?).
+     * - Space: O(?).
+     * @param {number} newVal The data to be added to a new node.
+     * @param {Node} curr The node that is currently accessed from the tree as
+     *    the tree is being traversed.
+     * @returns {BinarySearchTree} This tree.
+     */
+    insertRecursive(newVal, curr = this.root) { }
+
+    /**
+     * Determines if this tree contains the given searchVal.
+     * - Time: O(h) linear, h = height of tree.
+     * - Space: O(1) constant.
      * @param {number} searchVal The number to search for in the node's data.
      * @returns {boolean} Indicates if the searchVal was found.
      */
     contains(searchVal) {
-        if (this.isEmpty()){
-            return false;
-        }
-        let runner = this.root;
-        while (runner != null) {
-            if (runner.data === searchVal) {
+        let current = this.root;
+
+        while (current) {
+            if (current.data === searchVal) {
                 return true;
             }
-            else if (searchVal > runner.data) {
-                runner = runner.right;
-            }
-            else {
-                runner = runner.left;
+
+            if (searchVal < current.data) {
+                current = current.left;
+            } else {
+                current = current.right;
             }
         }
         return false;
@@ -66,31 +86,35 @@ class BinarySearchTree {
 
     /**
      * Determines if this tree contains the given searchVal.
-     * - Time: O(?).
-     * - Space: O(?).
+     * - Time: O(h) linear, h = height of tree.
+     * - Space: O(h) linear due to the call stack.
      * @param {number} searchVal The number to search for in the node's data.
      * @returns {boolean} Indicates if the searchVal was found.
      */
     containsRecursive(searchVal, current = this.root) {
-        if (!current) {
+        if (current === null) {
             return false;
         }
-        else if (current.data === searchVal) {
+
+        if (current.data === searchVal) {
             return true;
         }
-        else if (searchVal > current.data) {
-            return this.containsRecursive(searchVal, current.right);
-        }
-        else if (searchVal < current.data) {
+
+        if (searchVal < current.data) {
             return this.containsRecursive(searchVal, current.left);
+        }
+
+        if (searchVal > current.data) {
+            return this.containsRecursive(searchVal, current.right);
         }
     }
 
     /**
      * Calculates the range (max - min) from the given startNode.
-     * - Time: O(?).
-     * - Space: O(?).
-     * @param {Node} startNode The node to start from to calculate the range.
+     * - Time: O(rightHeight + leftHeight) -> still linear so simplify to O(h).
+     * - Space: O(h) linear due to the call stack. The max side finishes before
+     *    the right min side is added to the stack.
+     * @param {BSTNode} startNode The node to start from to calculate the range.
      * @returns {number|null} The range of this tree or a sub tree depending on if the
      *    startNode is the root or not.
      */
@@ -219,7 +243,6 @@ const twoLevelTree = new BinarySearchTree();
 twoLevelTree.root = new BSTNode(10);
 twoLevelTree.root.left = new BSTNode(5);
 twoLevelTree.root.right = new BSTNode(15);
-console.log(twoLevelTree.containsRecursive(5));
 
 /* threeLevelTree 
     root
---
+++ CSharp/Week_02/Day_03/LoginAndReg/Controllers/HomeController.cs
@@ -0,0 +1,110 @@
+﻿// Using statements
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using LoginAndReg.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace LoginAndReg.Controllers;
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private MyContext _context;
+    public HomeController(ILogger<HomeController> logger, MyContext context)
+    {
+        _logger = logger;
+        _context = context;
+    }
+
+    [HttpGet("")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost("users/create")]
+    public IActionResult CreateUser(User newUser)
+    {
+        if (ModelState.IsValid)
+        {
+            // Hash Passwords
+            PasswordHasher<User> Hasher = new PasswordHasher<User>();
+            newUser.Password = Hasher.HashPassword(newUser, newUser.Password);
+            _context.Add(newUser);
+            _context.SaveChanges();
+            HttpContext.Session.SetInt32("UserId", newUser.UserId);
+            return RedirectToAction("Success");
+        }
+        else
+        {
+            return View("Index");
+        }
+    }
+
+    [SessionCheck]
+    [HttpGet("success")]
+    public IActionResult Success()
+    {
+        return View();
+    }
+
+    [HttpPost("users/login")]
+    public IActionResult LoginUser(LogUser loginUser)
+    {
+        if (ModelState.IsValid)
+        {
+            // look up user in DB
+            User? userInDb = _context.Users.FirstOrDefault(u => u.Email == loginUser.LEmail);
+            // Check to see if user exists
+            if (userInDb == null)
+            {
+                ModelState.AddModelError("LEmail", "Invalid Email/Password");
+                return View("Index");
+            }
+            // Verify password matches whats in the DB
+            PasswordHasher<LogUser> hasher = new PasswordHasher<LogUser>();
+            var result = hasher.VerifyHashedPassword(loginUser, userInDb.Password, loginUser.LPassword);
+            if (result == 0)
+            {
+                // Failure
+                ModelState.AddModelError("LEmail", "Invalid Email/Password");
+                return View("Index");
+            }
+            else
+            {
+                // Set session and redirect to success
+                HttpContext.Session.SetInt32("UserId", userInDb.UserId);
+                return RedirectToAction("Success");
+            }
+        }
+        else
+        {
+            return View("Index");
+        }
+    }
+
+    [HttpPost("logout")]
+    public IActionResult Logout()
+    {
+        HttpContext.Session.Clear();
+        return RedirectToAction("Index");
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
+public class SessionCheckAttribute : ActionFilterAttribute
+{
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+        int? userId = context.HttpContext.Session.GetInt32("UserId");
+        if (userId == null)
+        {
+            context.Result = new RedirectToActionResult("Index", "Home", null);
+        }
+    }
+}
