06cc5c07cad6d7bf1542ab2eb234249e8f0aec63
Cliff Helms <cliff.helms.2@gmail.com>
2022-12-15 10:00:43 -0600

    update

--- CSharp/Week_03/Day_01/WeddingPlanner/Controllers/HomeController.cs
+++ CSharp/Week_03/Day_01/WeddingPlanner/Controllers/HomeController.cs
@@ -96,6 +96,7 @@ public class HomeController : Controller
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 
+// Create actual site =>
     [SessionCheck]
     [HttpGet("weddings")]
     public IActionResult Dashboard()
